258bd85e5eb18a742709e376346612fa08b62129
DevPrince3 <princekumar454500@gmail.com>
2026-09-20 22:40:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Interleave the First Half of the Queue with Second Half/interleave-the-first-half-of-the-queue-with-second-half.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void rearrangeQueue(Queue<Integer> q) {
+        Queue<Integer> q1=new LinkedList<>();
+        int n=q.size();
+        for(int i=1;i<=n/2;i++){
+            q1.add(q.remove());
+        }
+        while(q1.size()>0){
+            q.add(q1.remove());
+            q.add(q.remove());
+        }
+    }
+}
